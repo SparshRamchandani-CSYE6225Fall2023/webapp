153f740ddf982af75e809ee7847383e62910401f
sparshramchandani-NEU <ramchandani.sp@northeastern.edu>
2023-10-23 11:47:06 -0400

    RDS implemented

--- sequelize.js
+++ sequelize.js
@@ -1,17 +1,26 @@
 import dbConfigs from "./config/dbConfig.js";
 import { Sequelize } from "sequelize";
 
+const isAwsRDS = process.env.PGHOST && process.env.PGHOST.includes('.rds.amazonaws.com');
+const sequelizeConfig = {
+    dialect: 'postgres', 
+    host: process.env.DB_HOST,
+  };
+
+  if (isAwsRDS) {
+    sequelizeConfig.dialectOptions = {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    };
+  }
+
 const sequelize= new Sequelize(
     dbConfigs.DB,
     dbConfigs.USER, 
     dbConfigs.PASSWORD,
-    {
-        host:dbConfigs.HOST,
-        dialect:dbConfigs.dialect,
-        operatorsAliases:false,
-        operatorsAliases: 0,
-        pool:dbConfigs.pool
-    }
+    sequelizeConfig
 );
 
 export default sequelize;
