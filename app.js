81bd441a0b0874b50a8dbfef71a970ddd1cf7c4f
sparshramchandani-NEU <ramchandani.sp@northeastern.edu>
2023-11-06 13:05:41 -0500

    Logger Functionality

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import appRoute from './routes/app.route.js';
 import assignmentRoute from './routes/assignment.route.js';
+import logger from './configs/logger.config.js';
+import StatsD from 'node-statsd';
+
+const statsd = new StatsD({ host: 'localhost', port: 8125 }); // Adjust the host and port as needed
 
 const app=express();
 const PORT= process.env.PORT || 3000;
@@ -9,6 +13,7 @@ app.use("/healthz",appRoute);
 app.use("/v1/assignments", assignmentRoute);
 // app.use("/",(req,res)=>res.status(503).send())
 app.listen(PORT,(err)=>{
+    logger.info("logs from app.js");
     if(err){
         console.log("Failed to start the application")
     }else{
@@ -21,3 +26,5 @@ app.get('/', function(req, res){
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello World\n");
   });
+
+statsd.increment('app.start');
