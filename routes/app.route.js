073a7dda4dffb95ec3f35be1cff70380772e60a1
sparshramchandani-NEU <ramchandani.sp@northeastern.edu>
2023-11-07 14:13:11 -0500

    Merging to the org main branch

--- routes/app.route.js
+++ routes/app.route.js
@@ -1,30 +1,34 @@
 import sequelize from "../sequelize.js";
 import logger from "../configs/logger.config.js";
-import StatsD from 'node-statsd';
+import StatsD from "node-statsd";
 
-const statsd = new StatsD({ host: 'localhost', port: 8125 }); // Adjust the host and port as needed
+const statsd = new StatsD({ host: "localhost", port: 8125 }); // Adjust the host and port as needed
 
 const checkHealth = async (req, res) => {
-  if (req.method !== 'GET'){
-    logger.warn('Method not allowed');
-    return res.status(405).send();
-  }
-  const length = req.headers['content-length'];
-  if ((req.method === 'GET' && length > 0) || req.url.includes('?')) {
-    logger.error('Bad Request on health check, Query parameters not allowed');
-    return res.status(400).send();
-  }
-   else {
-    try {
-      await sequelize.authenticate();
-      logger.info('Health check successful');
-      statsd.increment('endpoint.health');
-      res.set('Cache-control', 'no-cache');
-      return res.status(200).send();
-    } catch (error) {
-      logger.fatal('Health check failed', error);
-      return res.status(503).send();
+  try {
+    if (req.method !== "GET") {
+      logger.warn("Method not allowed");
+      return res.status(405).send();
     }
+    const length = req.headers["content-length"];
+    if ((req.method === "GET" && length > 0) || req.url.includes("?")) {
+      logger.error("Bad Request on health check, Query parameters not allowed");
+      return res.status(400).send();
+    } else {
+      try {
+        await sequelize.authenticate();
+        logger.info("Health check successful");
+        statsd.increment("endpoint.health");
+        res.set("Cache-control", "no-cache");
+        return res.status(200).send();
+      } catch (error) {
+        logger.fatal("Health check failed", error);
+        return res.status(503).send();
+      }
+    }
+  } finally {
+    // Close the statsd connection to avoid open handles
+    statsd.close();
   }
 };
 
