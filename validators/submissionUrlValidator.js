0f24379c45dcf4c1f9cc5400a50c1f534db97ec5
sparshramchandani-NEU <ramchandani.sp@northeastern.edu>
2023-11-28 13:08:20 -0500

    Assignment 9 API

--- validators/submissionUrlValidator.js
+++ validators/submissionUrlValidator.js
@@ -1,4 +1,4 @@
 export default function submissionUrlValidator(submissionUrl) {
-    const githubRepoZipUrlRegex = /^https:\/\/github\.com\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]\/[a-zA-Z0-9_-]\/[a-zA-Z0-9_-]\/[a-zA-Z0-9._-]+\.zip$/;
+    const githubRepoZipUrlRegex = /^https:\/\/github\.com\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9._-]+\.zip$/;
     return githubRepoZipUrlRegex.test(submissionUrl);
   }
